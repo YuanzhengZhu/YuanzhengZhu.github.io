1f6709dc697f55f9e498d32b1d82322cd3180fc5
YuanzhengZhu <yzhu254@student.monash.edu>
2017-06-03 20:45:15 +1000

    final change

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,33 +1,267 @@
 $(document).ready(function() {
 
-  // Add jQuery here
 
-  // On .nav click, toggleClass ON or OFF showme.
+// Add jQuery here
 
-  $(document).ready(function() {
-    // Add jQuery here
+$('.l').mouseenter(function() {
+  $('.l__image').addClass('showme');
+});
+$('.l').mouseleave(function() {
+  $('.l__image').removeClass('showme');
+});
+$('.ll').mouseenter(function() {
+  $('.ll__image').addClass('showme');
+});
+$('.ll').mouseleave(function() {
+  $('.ll__image').removeClass('showme');
+});
+$('.lll').mouseenter(function() {
+  $('.lll__image').addClass('showme');
+});
+$('.lll').mouseleave(function() {
+  $('.lll__image').removeClass('showme');
+});
+$('.llll').mouseenter(function() {
+  $('.llll__image').addClass('showme');
+});
+$('.llll').mouseleave(function() {
+  $('.llll__image').removeClass('showme');
+});
+$('.h').mouseenter(function() {
+  $('.h__image').addClass('showme');
+});
+$('.h').mouseleave(function() {
+  $('.h__image').removeClass('showme');
+});
+$('.bacon').mouseenter(function() {
+  $('.bacon__image').addClass('showme');
+});
+$('.bacon').mouseleave(function() {
+  $('.bacon__image').removeClass('showme');
+});
+$('.cry').mouseenter(function() {
+  $('.cry__image').addClass('showme');
+});
+$('.cry').mouseleave(function() {
+  $('.cry__image').removeClass('showme');
+});
+$('.wheel').mouseenter(function() {
+  $('.wheel__image').addClass('showme');
+});
+$('.wheel').mouseleave(function() {
+  $('.wheel__image').removeClass('showme');
+});
+$('.avocado').mouseenter(function() {
+  $('.avocado__image').addClass('showme');
+});
+$('.avocado').mouseleave(function() {
+  $('.avocado__image').removeClass('showme');
+});
+$('.kebab').mouseenter(function() {
+  $('.kebab__image').addClass('showme');
+});
+$('.kebab').mouseleave(function() {
+  $('.kebab__image').removeClass('showme');
+});
+$('.woman').mouseenter(function() {
+  $('.woman__image').addClass('showme');
+});
+$('.woman').mouseleave(function() {
+  $('.woman__image').removeClass('showme');
+});
+$('.glass').mouseenter(function() {
+  $('.glass__image').addClass('showme');
+});
+$('.glass').mouseleave(function() {
+  $('.glass__image').removeClass('showme');
+});
+$('.glove').mouseenter(function() {
+  $('.glove__image').addClass('showme');
+});
+$('.glove').mouseleave(function() {
+  $('.glove__image').removeClass('showme');
+});
+$('.bread').mouseenter(function() {
+  $('.bread__image').addClass('showme');
+});
+$('.bread').mouseleave(function() {
+  $('.bread__image').removeClass('showme');
+});
+$('.honey').mouseenter(function() {
+  $('.honey__image').addClass('showme');
+});
+$('.honey').mouseleave(function() {
+  $('.honey__image').removeClass('showme');
+});
+$('.egg').mouseenter(function() {
+  $('.egg__image').addClass('showme');
+});
+$('.egg').mouseleave(function() {
+  $('.egg__image').removeClass('showme');
+});
+$('.baguette').mouseenter(function() {
+  $('.baguette__image').addClass('showme');
+});
+$('.baguette').mouseleave(function() {
+  $('.baguette__image').removeClass('showme');
+});
+$('.pancake').mouseenter(function() {
+  $('.pancake__image').addClass('showme');
+});
+$('.pancake').mouseleave(function() {
+  $('.pancake__image').removeClass('showme');
+});
+$('.salad').mouseenter(function() {
+  $('.salad__image').addClass('showme');
+});
+$('.salad').mouseleave(function() {
+  $('.salad__image').removeClass('showme');
+});
+$('.g').mouseenter(function() {
+  $('.g__image').addClass('showme');
+});
+$('.g').mouseleave(function() {
+  $('.g__image').removeClass('showme');
+});
+$('.deer').mouseenter(function() {
+  $('.deer__image').addClass('showme');
+});
+$('.deer').mouseleave(function() {
+  $('.deer__image').removeClass('showme');
+});
+$('.fox').mouseenter(function() {
+  $('.fox__image').addClass('showme');
+});
+$('.fox').mouseleave(function() {
+  $('.fox__image').removeClass('showme');
+});
+$('.r').mouseenter(function() {
+  $('.r__image').addClass('showme');
+});
+$('.r').mouseleave(function() {
+  $('.r__image').removeClass('showme');
+});
+$('.bat').mouseenter(function() {
+  $('.bat__image').addClass('showme');
+});
+$('.bat').mouseleave(function() {
+  $('.bat__image').removeClass('showme');
+});
+$('.eagle').mouseenter(function() {
+  $('.eagle__image').addClass('showme');
+});
+$('.eagle').mouseleave(function() {
+  $('.eagle__image').removeClass('showme');
+});
+$('.duck').mouseenter(function() {
+  $('.duck__image').addClass('showme');
+});
+$('.duck').mouseleave(function() {
+  $('.duck__image').removeClass('showme');
+});
+$('.owl').mouseenter(function() {
+  $('.owl__image').addClass('showme');
+});
+$('.owl').mouseleave(function() {
+  $('.owl__image').removeClass('showme');
+});
+$('.lizard').mouseenter(function() {
+  $('.lizard__image').addClass('showme');
+});
+$('.lizard').mouseleave(function() {
+  $('.lizard__image').removeClass('showme');
+});
+$('.shark').mouseenter(function() {
+  $('.shark__image').addClass('showme');
+});
+$('.shark').mouseleave(function() {
+  $('.shark__image').removeClass('showme');
+});
+$('.shrimp').mouseenter(function() {
+  $('.shrimp__image').addClass('showme');
+});
+$('.shrimp').mouseleave(function() {
+  $('.shrimp__image').removeClass('showme');
+});
+$('.squid').mouseenter(function() {
+  $('.squid__image').addClass('showme');
+});
+$('.squid').mouseleave(function() {
+  $('.squid__image').removeClass('showme');
+});
+$('.butter').mouseenter(function() {
+  $('.butter__image').addClass('showme');
+});
+$('.butter').mouseleave(function() {
+  $('.butter__image').removeClass('showme');
+});
+$('.m').mouseenter(function() {
+  $('.m__image').addClass('showme');
+});
+$('.m').mouseleave(function() {
+  $('.m__image').removeClass('showme');
+});
+$('.scooter').mouseenter(function() {
+  $('.scooter__image').addClass('showme');
+});
+$('.scooter').mouseleave(function() {
+  $('.scooter__image').removeClass('showme');
+});
+$('.c').mouseenter(function() {
+  $('.c__image').addClass('showme');
+});
+$('.c').mouseleave(function() {
+  $('.c__image').removeClass('showme');
+});
+$('.boxing').mouseenter(function() {
+  $('.boxing__image').addClass('showme');
+});
+$('.boxing').mouseleave(function() {
+  $('.boxing__image').removeClass('showme');
+});
+$('.handball').mouseenter(function() {
+  $('.handball__image').addClass('showme');
+});
+$('.handball').mouseleave(function() {
+  $('.handball__image').removeClass('showme');
+});
+$('.fencing').mouseenter(function() {
+  $('.fencing__image').addClass('showme');
+});
+$('.fencing').mouseleave(function() {
+  $('.fencing__image').removeClass('showme');
+});
+$('.j').mouseenter(function() {
+  $('.j__image').addClass('showme');
+});
+$('.j').mouseleave(function() {
+  $('.j__image').removeClass('showme');
+});
+$('.polo').mouseenter(function() {
+  $('.polo__image').addClass('showme');
+});
+$('.polo').mouseleave(function() {
+  $('.polo__image').removeClass('showme');
+});
+$('.palm').mouseenter(function() {
+  $('.palm__image').addClass('showme');
+});
+$('.palm').mouseleave(function() {
+  $('.palm__image').removeClass('showme');
+});
+$('.thinking').mouseenter(function() {
+  $('.thinking__image').addClass('showme');
+});
+$('.thinking').mouseleave(function() {
+  $('.thinking__image').removeClass('showme');
+});
+$('.cro').mouseenter(function() {
+  $('.cro__image').addClass('showme');
+});
+$('.cro').mouseleave(function() {
+  $('.cro__image').removeClass('showme');
+});
 
-    $(".rollover").mouseenter(function() {
-      $(".blur").addClass("showme");
-    });
 
-    $(".rollover").mouseleave(function() {
-      $(".blur").removeClass("showme");
-    });
 
-    $(".magazine").mouseenter(function() {
-      $(".magazine__image").addClass("showme");
-    });
-
-    $(".magazine").mouseleave(function() {
-      $(".magazine__image").removeClass("showme");
-    });
-
-    $(".landscape").mouseenter(function() {
-      $(".landscape__image").addClass("showme");
-    });
-
-    $(".landscape").mouseleave(function() {
-      $(".landscape__image").removeClass("showme");
-    });
   });
